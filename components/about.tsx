c17b195dd240407fe6a4334c5b1578e72bed9e49
Oksy888 <ylarchenko.bb@gmail.com>
2025-02-18 22:51:09 +0200

    Revise About component content for clarity and detail; update personal journey and skills

--- components/about.tsx
+++ components/about.tsx
@@ -19,30 +19,25 @@ export default function About() {
     >
       <SectionHeading>About me</SectionHeading>
       <p className="mb-3">
-        My journey into web development began at university, where we developed
-        a CRM for a business (php ui + postgreSql db). After earning my degree
-        in <span className="font-medium">Econimic cybernetics</span>, I decided
-        to follow my passion for programming. I attended a coding bootcamp and
-        learned <span className="font-medium">full-stack web development</span>.{' '}
-        <span className="italic">What I enjoy most about programming</span> is
-        solving problems. I <span className="underline">love</span> the
-        satisfaction of finding solutions. My core stack includes{' '}
-        <span className="font-medium">
-          React, Next.js, Node.js, and MongoDB
-        </span>
-        . I am also proficient in TypeScript and Prisma. I am always eager to
-        learn new technologies and am currently seeking a{' '}
+        My journey into web development began at university, where I developed a
+        system for managing inventory, purchasers, and orders, utilizing a
+        PHP-based interface with a PostgreSQL database ( accent was more on the
+        creating{' '}
+        <span className="font-medium"> normalized and proper database</span>
+        ). After earning my degree in{' '}
+        <span className="font-medium">Economic cybernetics</span>, began my
+        career in Quality Assurance before transitioning into front-end
+        development. I advanced my skills by studying JavaScript and TypeScript,
+        specializing in the React framework. For the past two years, I have been
+        working as a Front-End Developer at Gromus AI, contributing to the
+        development of web applications. I am highly motivated, committed to
+        continuous learning, and eager to enhance my expertise in front-end
+        development. My core stack includes{' '}
+        <span className="font-medium">React, Next.js</span>. I am also have
+        skills in <span className="font-medium">TypeScript and Prisma.</span> I
+        am always eager to learn new technologies and am currently seeking a{' '}
         <span className="font-medium">full-time position</span> as a software
         developer.
-      </p>
-
-      <p>
-        <span className="italic">When I'm not coding</span>, I enjoy playing
-        video games, watching movies, and playing with my dog. I also enjoy{' '}
-        <span className="font-medium">learning new things</span>. I am currently
-        learning about{' '}
-        <span className="font-medium">history and philosophy</span>. I'm also
-        learning how to play the guitar.
       </p>
     </motion.section>
   )
