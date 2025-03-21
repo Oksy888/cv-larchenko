904c60f654bc28b4a6ea840c2abe27966bb37b1f
Oksy888 <ylarchenko.bb@gmail.com>
2025-03-21 19:39:19 +0200

    Add project links to Project component and update projects data with new entries

--- lib/data.ts
+++ lib/data.ts
@@ -6,6 +6,9 @@ import youTubeImg from '@/public/yt.png'
 import webSockets from '@/public/webSockets.png'
 import profile from '@/public/profile.png'
 import marvel from '@/public/marvel.png'
+import videoImg from '@/public/video.png'
+import bpsImg from '@/public/bps.png'
+import analyticsImg from '@/public/analytics.png'
 
 export const links = [
   {
@@ -66,15 +69,49 @@ export const projectsData = [
     title: 'You Tube Clone',
     description:
       'Practice project to clone the YouTube homepage. It has features like collapsible sidebar, responsive design, day and time formatting, using ResizeObserver for pills bar.',
-    tags: ['React', 'TypeScript', 'Tailwind', 'CSS Grid'],
+    tags: ['React', 'TypeScript', 'JS', 'Tailwind', 'CSS Grid'],
     imageUrl: youTubeImg,
+    link: 'https://youtube-clone-bay-three.vercel.app/',
   },
   {
     title: 'Web Sockets',
     description:
       'A chat app that uses web sockets to send messages in real-time. It has features like presets, message history, like/dislike of answer.',
-    tags: ['React', 'TypeScript', 'Mantine UI', 'Zustand', 'React Query'],
+    tags: ['React', 'TypeScript', 'JS', 'Mantine UI', 'Zustand', 'React Query'],
     imageUrl: webSockets,
+    link: 'https://web-socets.vercel.app',
+  },
+  {
+    title: 'Video generator',
+    description:
+      'A video generator that uses AI to generate videos and FFmpeg to join video and audio. It has features like authorization, video preview, download audio and video.',
+    tags: ['React', 'TypeScript', 'JS', 'Mantine UI', 'Zustand', 'React Query'],
+    imageUrl: videoImg,
+    link: 'https://video.gromus.ai/',
+  },
+  {
+    title: 'Best Part of Song',
+    description:
+      'A project that uses AI for slicing sounds on most useful parts for artists. It has features like play, download song part, pagination, infinity scroll and a detail page.',
+    tags: ['React', 'TypeScript', 'JS', 'Mantine UI', 'Zustand', 'React Query'],
+    imageUrl: bpsImg,
+    link: 'https://bps.gromus.ai/',
+  },
+  {
+    title: 'Gromus',
+    description:
+      'AI tool to be viral on TikTok. It has features like user authorization, tanStack table, tanStack query, internationalization, filters, useContext, state management Zustand.',
+    tags: [
+      'React',
+      'TypeScript',
+      'JS',
+      'Mantine UI',
+      'Zustand',
+      'React Query',
+      'React-i18next',
+    ],
+    imageUrl: analyticsImg,
+    link: 'https://gromus.ai/',
   },
   {
     title: 'Profile',
@@ -82,6 +119,7 @@ export const projectsData = [
       'This website is built with Next.js and Tailwind. It has a dark mode, is fully responsive and is used MDX markdown for dynamic content.',
     tags: ['React', 'Next.js', 'TypeScript', 'Tailwind', 'MDX'],
     imageUrl: profile,
+    link: 'https://profile-lyart-two-20.vercel.app/',
   },
   {
     title: 'Marvel',
@@ -89,6 +127,7 @@ export const projectsData = [
       'A project to practice fetching data from an API. It has features like pagination, infinity scroll and a detail page.',
     tags: ['React', 'JS', 'SCSS'],
     imageUrl: marvel,
+    link: 'https://marvel-nine-snowy.vercel.app/',
   },
 ] as const
 
